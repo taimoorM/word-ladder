88e2cf4857e2c6022f676d10d3b50e492f7445cf
Taimoor <taimoor.m89@gmail.com>
2023-04-02 18:58:35 -0400

    Add: Basic form and api fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
+
 function App() {
+  const [startWord, setStartWord] = useState<string>("");
+  const [endWord, setEndWord] = useState<string>("");
+  const [wordLadder, setWordLadder] = useState([]);
+
+  const handleStartWordChange = (event: any): void => {
+    setStartWord(event.target.value);
+  };
+
+  const handleEndWordChange = (event: any) => {
+    setEndWord(event.target.value);
+  };
+
+  const handleGenerateWordLadder = async () => {
+    // Connect to API to get list of valid English words
+    const response = await fetch(`https://random-word-api.herokuapp.com/word`);
+    const data = await response.json();
+    const validWords = data;
+    console.log(validWords);
+  };
+
   return (
     <div>
-      <h1 className="4xl uppercase">hello world!</h1>
+      <label>
+        Starting word:
+        <input type="text" value={startWord} onChange={handleStartWordChange} />
+      </label>
+      <label>
+        Ending word:
+        <input type="text" value={endWord} onChange={handleEndWordChange} />
+      </label>
+      <button onClick={handleGenerateWordLadder}>Generate Word Ladder</button>
+      {wordLadder.length > 0 && (
+        <ul>
+          {wordLadder.map((word) => (
+            <li>{word}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
