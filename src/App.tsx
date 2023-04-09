f3091591d8e8141a97daa601c9c85dc73b82690a
Taimoor <taimoor.m89@gmail.com>
2023-04-09 19:01:29 -0400

    Add: score and change dictionary api

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,28 @@ type letterPositionType = number | null;
 
 function App() {
   const [words, setWords] = useState<string[][]>([]);
-  const [wordBoard, setWordBoard] = useState<string[]>([]);
+  const [wordBoard, setWordBoard] = useState<
+    { definition: string; word: string; valid: boolean }[]
+  >([]);
   const [wordInput, setWordInput] = useState<string>("");
+  const [score, setScore] = useState<number>(0);
 
   const handleWordSubmit = async () => {
     const response = await fetch(
-      `https://api.dictionaryapi.dev/api/v2/entries/en/${wordInput}`
+      `https://api.api-ninjas.com/v1/dictionary?word=${wordInput}`,
+      {
+        headers: { "X-Api-Key": import.meta.env.VITE_API_NINJAS_API_KEY },
+      }
     );
     const data = await response.json();
     console.log(data);
-    if (data.title === "No Definitions Found") {
-      console.log("No Definitions Found");
+    if (!data.valid) {
+      console.log("Word does not exist");
     } else {
-      setWordBoard([...wordBoard, wordInput]);
-      console.log(wordBoard);
+      setWordBoard([...wordBoard, data]);
+      console.log(data);
       setWordInput("");
+      setScore(score + wordInput.length);
     }
   };
 
@@ -47,12 +54,15 @@ function App() {
         <h1 className="text-4xl text-center">Wordzo</h1>
       </div>
       <div className="flex w-full justify-center">
-        <div className="flex flex-col mr-32 w-[150px] border border-gray-800 p-2 space-y-2">
-          {wordBoard.map((word, index) => (
-            <div>
-              <span>{index + 1}.</span> {word}
-            </div>
-          ))}
+        <div className="flex flex-col h-full">
+          <div>Score: {score}</div>
+          <div className="flex flex-col mr-32 w-[150px] border border-gray-800 p-2 space-y-2 h-full">
+            {wordBoard.map((word, index) => (
+              <div>
+                <span>{index + 1}.</span> {word.word}
+              </div>
+            ))}
+          </div>
         </div>
         <div>
           <div className="h-10 flex justify-center">{wordInput}</div>
@@ -71,29 +81,6 @@ function App() {
                 ))}
               </div>
             ))}
-
-            {/* <div className="flex space-x-2 mb-5">
-              {words[1].map((letter, index) => (
-                <div
-                  key={index}
-                  className="p-2 border-2 border-gray-600  w-10 h-10 flex justify-center items-center cursor-pointer"
-                  onClick={() => handleLetterClick(letter)}
-                >
-                  {letter}
-                </div>
-              ))}
-            </div>
-            <div className="flex space-x-2 mb-10">
-              {words.map((letter, index) => (
-                <div
-                  key={index}
-                  className="p-2 border-2 border-gray-600  w-10 h-10 flex justify-center items-center cursor-pointer"
-                  onClick={() => handleLetterClick(letter)}
-                >
-                  {letter}
-                </div>
-              ))}
-            </div> */}
             <div>
               <button onClick={handleWordSubmit}>Submit</button>
             </div>
